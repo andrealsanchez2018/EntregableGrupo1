4fd50056166fcfbe24a5a34f7e5249cf7d0d252b
japerezcarri <54781818+japerezcarri@users.noreply.github.com>
2020-05-02 10:00:19 -0500

    cambios en registro

--- vista/adminUsuario.php
+++ vista/adminUsuario.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 echo @$_SESSION['user']['nombre'];
-    
-require(__DIR__.'/../control/usuarioControl.php');
+
+require(__DIR__ . '/../control/usuarioControl.php');
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="es">
 
 <head>
@@ -13,118 +14,105 @@ require(__DIR__.'/../control/usuarioControl.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Clothing Bictia- Administración de Usuario</title>
- 
+
 
 </head>
 
 <body class="container">
-<!--script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script-->
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    <?php include'header.php'?>
+    <?php include 'header.php' ?>
 
     <!-- Formulario del producto -->
-   <center> <h2>Administración de Usuario</h2></center>
-    <section class="formulario" style="margin-top: 20px;" >
+    <center>
+        <h2>Administración de Usuario</h2>
+    </center>
+    <section class="formulario" style="margin-top: 20px;">
         <div class="row">
-            <div class="col-md-5 col-12">
-                <?php //include('registroUsuario.php')?>
-        <form>
-           
-            <div class="form-group row">
-              <label for="idusuario" class="col-sm-2 ">idUsuario</label>
-              <div class="col-sm-10">
-              <input class="form-control" type="number" id="idusuario" placeholder="Código de usuario" name="idusuario">
-              </div>
+            <div class="col-md-3 col-12">
+                <?php //include('registroUsuario.php')
+                ?>
+                <form>
+                    <div class="form-group row">
+                        <input class="form-control " type="number" id="idusuario" placeholder="Código de usuario" name="idusuario">
+                        <input class="form-control " type="text" id="nombre" placeholder="Nombre de usuario" name="nombre">
+                        <input class="form-control " type="text" id="correo" placeholder="Correo" name="correo">
+                        <input class="form-control " type="text" id="pass" placeholder="Contraseña" name="pass">
+                        <?php  if (@$_SESSION['user']['rol'] == 'administrador') :   ?>
+                        <select class="form-control" name="rol" id="rol" name="rol">
+                            <option value="3">Super Admin</option>
+                            <option value="2">Administrador</option>
+                            <option value="1">Usuario</option>
+                        </select>
+
+                        <?php ?>
+
+                        <select class="form-control" name="estado" id="estado" name="estado">
+                            <option value="1">Activo</option>
+                            <option value="0">Inactivo</option>
+                        </select>
+
+                        <?php endif;  ?>
+
+                        <center>
+                            <button type="submit" class="btn btn-light">Modificar</button>
+                            <button type="submit" class="btn btn-light">Cancelar</button>
+                        </center>
+                    </div>
+
+
+                </form>
             </div>
-            <div class="form-group row">
-                <label for="nombre" class="col-sm-2 col-form-label">Nombre</label>
-                <div class="col-sm-10">
-                <input class="form-control" type="text"  id="nombre" placeholder="Nombre de usuario" name="nombre">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="correo" class="col-sm-2 col-form-label">Correo</label>
-                <div class="col-sm-10">
-                <input class="form-control" type="text"  id="correo" placeholder="Correo" name="correo">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="pass" class="col-sm-2 col-form-label">password</label>
-                <div class="col-sm-10">
-                <input class="form-control" type="text"  id="pass" placeholder="Contraseña" name="pass">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="rol" class="col-sm-2 col-form-label">idRol</label>
-                <div class="col-sm-10">
-                <input class="form-control" type="number"  id="rol" placeholder="Rol del usuario" name="rol">
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="estado" class="col-sm-2 col-form-label">idEstado</label>
-                <div class="col-sm-10">
-                <input class="form-control" type="number"  id="estado" placeholder=" en Stock" name="estado">
-                </div>
-              </div>
+            <!-- sección de opciones -->
+            <div class="col-md-9 col-12">
+                <form class="form-inline my-2 ">
+                    <input class="buscar mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
 
-              
-          </form>
-        </div>
-        <!-- sección de opciones -->
-        <div class="col-md-7 col-12">
-             <form class="form-inline my-2 ">
-             <input class="buscar mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
-             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-             
-            </form>
-            <div class="table-responsive">
-            <table class="table table-hover">
-                <thead class="thead-ligth ">
-                    <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Correo</th>
-                    <th scope
-                    ="col">Contraseña</th>
-                    <th scope="col">idRol</th>
-                    <th scope="col">idEstado</th>
-                    <th scope="col">Opciones</th>
-                    </tr>
-                </thead>
-                <tbody id="result">
-                    <tr>
-                    <th scope="row">1</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><button>Modificar</button><button class="icon-trash-empty"></button></td>
-                    </tr>
-                    
-                    <tr>
-                    <th scope="row">2</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><button>Modificar</button><button class="icon-trash-empty"></button></td>
-                    </tr>
-                   
-                </tbody>
-            </table>
+                </form>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead class="thead-ligth ">
+                            <tr>
+                                <th scope="col">id</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Correo</th>
+                                <th scope="col">Contraseña</th>
+                                <th scope="col">idRol</th>
+                                <th scope="col">idEstado</th>
+                                <th scope="col">Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody id="result">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td><button>Modificar</button><button class="icon-trash-empty"></button></td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">2</th>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td><button>Modificar</button><button class="icon-trash-empty"></button></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+
+
             </div>
-
-
-        </div>
         </div>
     </section>
-   <?php include'footer.php' ?>
-<script type="text/javascript" src="../public/js/Async.js"></script>
+    <?php include 'footer.php' ?>
+    <script type="text/javascript" src="../public/js/Async.js"></script>
 </body>
 
 </html>
--- vista/registroUsuario.php
+++ vista/registroUsuario.php
@@ -2,39 +2,59 @@
 session_start();
 echo @$_SESSION['user']['nombre'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Usuario</title>
 </head>
-<body>
-    <div id="registroForm">
-        
-    <form method="POST" action="control/usuarioControl.php?accion=registrar">
-        <input class="form-control" type="text" name="nombre" id="nombre" placeholder="Nombre Completo">
-        <br>
-        <input type="text" name="correo" id="correo" placeholder="Correo">
-        <br>
-        <input type="pasword" name="pass" id="pass" placeholder="Contraseña">
-        <br>
-        
-        <?php if(@$_SESSION['user']['rol']=='administrador'):?>
-        <select name="rol" id="rol" name="rol">
-        <option value="3">Super Admin</option>
-        <option value="2">Administrador</option>
-        <option value="1">Usuario</option>
-        </select>
-        <?php ?>
-        <select name="estado" id="estado" name="estado">
-        <option value="1">Activo</option>
-        <option value="0">Inactivo</option>
-        </select>
-        <?php endif;?>
-        <input type="submit"  value="Registrar" name="enter" >
-    </form>
+
+<body class="container">
+    <?php include 'header.php'  ?>
+    <div class="d-flex justify-content-center h-100" id= "registroForm">
+        <div class="card">
+            <div class="card-header">
+                <h3>Registro</h3>
+                
+            </div>
+            <div class="card-body">
+                <form>
+                <form method="POST" action="control/usuarioControl.php?accion=registrar">
+                    <div></div>
+                    <input class="form-control " type="text" name="nombre" id="nombre" placeholder="Nombre Completo">
+
+                    <input class="form-control " type="text" name="correo" id="correo" placeholder="Correo">
+
+                    <input class="form-control " type="pasword" name="pass" id="pass" placeholder="Contraseña">
+
+                    <?php /*  if (@$_SESSION['user']['rol'] == 'administrador') :  */ ?>
+                    <select class="form-control" name="rol" id="rol" name="rol">
+                        <option value="3">Super Admin</option>
+                        <option value="2">Administrador</option>
+                        <option value="1">Usuario</option>
+                    </select>
+
+                    <?php ?>
+
+                    <select class="form-control" name="estado" id="estado" name="estado">
+                        <option value="1">Activo</option>
+                        <option value="0">Inactivo</option>
+                    </select>
+
+                    <?php /* endif; */ ?>
+                    <input type="submit" value="Registrar" name="enter" class="btn float-right login_btn">
+                </form>
+                </form>
+            </div>
+           
+        </div>
     </div>
     
+   
+    <?php include 'footer.php' ?>
 </body>
+
 </html>
