d1cea480bfb9fd6f55f8a55c90735da9db8b6d67
julianza32 <julianza32@gmail.com>
2020-05-04 17:10:40 -0500

    Estilos de Categoria

--- vista/categorias.php
+++ vista/categorias.php
@@ -1,24 +1,47 @@
-<?php include('../vista/header.php'); #prueba?>
+<?php include('../vista/header.php'); #prueba
+?>
 
 <body class="container">
     <?php include('../control/categoriaControl.php');
     $rol = checkRol();
     ?>
-    <form action="/EntregableGrupo1/control/categoriaControl.php" method="post">
-        <input type="text" id="c_id" name="c_id" placeholder="Id">
-        <br>
-        <input type="text" id="c_nombre" name="c_nombre" placeholder="nombre de la categoria">
-        <br>
-        <?php desplegableEstado($rol); ?>
-        <br>
-        <input type="submit" value="Registrar" name="c_enter">
-        <input type="submit" value="Actualizar" name="c_enter">
-        <input type="submit" value="Eliminar" name="c_enter">
-
+    <form action="/EntregableGrupo1/control/categoriaControl.php" method="post" class="w-50 m-auto">
+        <div class="form-group">
+            <label for="c_id">Id</label>
+            <input type="text" id="c_id" name="c_id" placeholder="Id" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="c_nombre">Nombre de categoria</label>
+            <input type="text" id="c_nombre" name="c_nombre" placeholder="nombre de la categoria" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="Estado">Estado</label>
+            <select id='Estado' name='c_estado' class='form-control'>
+            <?php desplegableEstado($rol); ?>
+            </select>
+        </div>
+        <div class="btn-group m-auto">
+            <input type="submit" class="btn btn-light" value="Registrar" name="c_enter">
+            <input type="submit" class="btn btn-light" value="Actualizar" name="c_enter">
+            <input type="submit" class="btn btn-light" value="Eliminar" name="c_enter">
+        </div>
     </form>
     <?php listarCategorias(); ?>
-    <?php listarCategoriasEstado();
-    desplegableCategoria(); ?>
+    <div class="table table-responsive m-auto w-50">
+        <table class="table">
+            <thead>
+                <th>Id</th>
+                <th>Categoria</th>
+                <th>Estado</th>
+                
+
+            </thead>
+            <tbody>
+                <?php listarCategoriasEstado($rol); ?>
+            </tbody>
+        </table>
+    </div>
+    <?php desplegableCategoria(); ?>
 
 </body>
 
