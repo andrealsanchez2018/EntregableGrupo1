b8940e1a46cad8b7589d4992e3a0f9e118ea6342
andrealsanchez2018 <andrealsanchez2018@gmail.com>
2020-05-04 14:39:43 -0500

    Adicion de estilos hasta categoria

--- modelo/class.Producto.php
+++ modelo/class.Producto.php
@@ -14,7 +14,7 @@ class Producto{
 
         $sql = "INSERT INTO productos (nombreProducto, precio, imgProducto, descripcion, cantidad, idCategoria, idSubCategoria, idEstado, idUsuario_cre, idUsuario_mod , fecha_mod, fecha_cre) VALUES ('$nombreProducto', '$precio', '$imgProducto', '$descripcion', '$cantidad', '$idCategoria', '$idSubCategoria', '$idEstado','$idUsuario_cre', '$idUsuario_mod', NOW(), NOW())";
         
-        $db->query($sql)  ? header('Location: ../vista/productos.php?res=Producto_Agregado') : header('Location: ../vista/productos.php?res=error') ;
+        $db->query($sql); /*  ? header('Location: ../vista/productos.php?res=Producto_Agregado') : header('Location: ../vista/productos.php?res=error') ; */
     }
 
     public function mostrarProductos(){
--- vista/categorias.php
+++ vista/categorias.php
@@ -4,7 +4,6 @@
     <?php include('../control/categoriaControl.php');
     $rol = checkRol();
     ?>
-    <?php include('header.php');?>
     <form action="/EntregableGrupo1/control/categoriaControl.php" method="post">
         <input type="text" id="c_id" name="c_id" placeholder="Id">
         <br>
